b6338560185f12e7ba8e0b8f29f7cefdf38e98b2
Erwan Bourre <github@bourre.me>
2025-08-16 20:19:56 +0200

    Using stylua fmt

--- lua/sql-autocomplete.lua
+++ lua/sql-autocomplete.lua
@@ -6,17 +6,16 @@ vim.g.autocompletels_data = "***"
 
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'teradata',
-  callback = function()
-    completion.setup()
-  end,
+    pattern = 'teradata',
+    callback = function()
+        completion.setup()
+    end,
 })
 
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'sql',
-  callback = function()
-    completion.setup()
-  end,
+    pattern = 'sql',
+    callback = function()
+        completion.setup()
+    end,
 })
-
